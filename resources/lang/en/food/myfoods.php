e31a47b0769e0c254daa64c3b4677508aaab9d15
Pablo Mérida <pablomerida2001@gmail.com>
2022-05-16 15:39:18 +0200

    add spanish tranlations

---
+++ resources/lang/en/food/myfoods.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | My foods Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'tittle' => 'My Foods',
+    'breakfast' => 'Breakfast',
+    'total' => 'Total',
+    'lunch' => 'Lunch',
+    'dinner' => 'Dinner',
+    'snacks' => 'Snacks',
+    'add' => 'Add food',
+    'add new' => 'Add new food',
+    'search' => 'Search...',
+    'quantity' => 'Quantity',
+    'meal' => 'Meal',
+    'date' => 'Date',
+    'save' => 'Save'
+];
